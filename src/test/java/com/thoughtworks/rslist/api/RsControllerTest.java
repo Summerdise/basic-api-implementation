0b4b4bcbe7e72f68b36267010a63d415b6559542
Summerdise <350464517@qq.com>
2020-09-16 18:05:20 +0800

    [zmc] fix: fix test file into api test

---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,90 @@
+package com.thoughtworks.rslist.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.dto.RsItem;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class RsControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getOneRsItemFromList() throws Exception {
+        mockMvc.perform(get("/rs/0"))
+                .andExpect(content().string("第一条事件,1"));
+    }
+
+    @Test
+    void getRsItemsFromListByStartNumber() throws Exception {
+        mockMvc.perform(get("/rs/list?start=0&end=2"))
+                .andExpect(content().string("[第一条事件,1, 第二条事件,2]"));
+        mockMvc.perform(get("/rs/list?start=0&end=3"))
+                .andExpect(content().string("[第一条事件,1, 第二条事件,2, 第三条事件,3]"));
+    }
+
+    @Test
+    void insertItemIntoList() throws Exception {
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[第一条事件,1, 第二条事件,2, 第三条事件,3]"));
+        RsItem rsItem = new RsItem("第四条事件", "4");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(rsItem);
+        mockMvc.perform(post("/rs/all").content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[第一条事件,1, 第二条事件,2, 第三条事件,3, 第四条事件,4]"));
+    }
+
+    @Test
+    void fixItemInList() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        RsItem noNameRsItem = new RsItem(null, "修改keyword-1");
+        String noNameJson = objectMapper.writeValueAsString(noNameRsItem);
+        mockMvc.perform(post("/rs/fix/0").content(noNameJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[第一条事件,修改keyword-1, 第二条事件,2, 第三条事件,3]"));
+
+        RsItem noKeywordRsItem = new RsItem("修改name-2", null);
+        String noKeywordJson = objectMapper.writeValueAsString(noKeywordRsItem);
+        mockMvc.perform(post("/rs/fix/0").content(noKeywordJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[修改name-2,修改keyword-1, 第二条事件,2, 第三条事件,3]"));
+
+        RsItem fixRsItem = new RsItem("修改name-3", "修改keyword-3");
+        String fixJson = objectMapper.writeValueAsString(fixRsItem);
+        mockMvc.perform(post("/rs/fix/1").content(fixJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[修改name-2,修改keyword-1, 修改name-3,修改keyword-3, 第三条事件,3]"));
+    }
+
+    @Test
+    void deleteItemInList() throws Exception {
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[第一条事件,1, 第二条事件,2, 第三条事件,3]"));
+        mockMvc.perform(get("/rs/delete/1")).andExpect(status().isOk());
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[第一条事件,1, 第三条事件,3]"));
+        mockMvc.perform(get("/rs/delete/2")).andExpect(status().isOk());
+        mockMvc.perform(get("/rs/all"))
+                .andExpect(content().string("[第一条事件,1, 第三条事件,3]"));
+    }
+}
